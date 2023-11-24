54621ef27124aa75092e6933bc3ec4368d9a7a08
Zane Estere Gruntmane <141255754+esterzane@users.noreply.github.com>
2023-11-24 12:31:38 +0000

    todo list fetch submit, have to solve the issue related to the last element which is being deleted after reload

--- src/js/component/YourTodoList.jsx
+++ src/js/component/YourTodoList.jsx
@@ -2,13 +2,20 @@ import React, { useEffect, useState } from "react";
 
 
 const YourTodoList = () => {
-	const [todoList, setTodoList] = useState([{ label: "What's up?", done: false }])
+	const [todoList, setTodoList] = useState([])
 	const [newTask, setNewTask] = useState("")
 	const ENDPOINT = "https://playground.4geeks.com/apis/fake/todos/user/zaneestere";
 
+
 	const handleFetchTodoList = async () => {
 		const todoResponse = await fetch(ENDPOINT);
-
+		fetch(ENDPOINT, {
+			method: "POST",
+			headers: {
+				"Content-Type": "application/json",
+			},
+			body: JSON.stringify([])
+		})
 		if (todoResponse.ok) {
 			const getResponse = await fetch(ENDPOINT);
 			const todoJson = await getResponse.json();
@@ -56,35 +63,48 @@ const YourTodoList = () => {
 	return (
 		<div className="todo-list container-fluid mt-5 mb-5 text-center">
 
-			<form className="todo-form" onSubmit={(e) => handleSubmit(e)}>
+			<form className="todo-form mt-5" onSubmit={(e) => handleSubmit(e)}>
 				<h1 className="list-title"> Your todo list <i className="fa-solid fa-arrows-turn-to-dots" style={{ color: "#e1e8f5" }}></i> </h1>
-				<input className="text-input" type="text" value={newTask} onChange={(e) => handleChange(e)} placeholder="What do you want to do?..." />
-				<input className="button btn btn-secondary rounded-1 p-1" type="submit" value={"Add task"} />
+				<input className="text-input rounded" type="text" value={newTask} onChange={(e) => handleChange(e)} placeholder="What do you want to do?..." />
+				<input className="button btn btn-secondary btn-sm rounded p-0.1 m-2" type="submit" value={"Add task"} />
 			</form>
 
 			{todoList.length > 0 ? (
-				<div class="container-fluid">
+				<div className="container-fluid">
 					<div className="todo-items justify-content-around align-items-center bg-light shadow-sm bg-body-tertiary rounded m-auto” style={{ width: “30rem” }}">
 
 						{todoList.map((task, index) => (
 							<div className="icon-container" key={index}>
-								<div>
-									<i className="fa-regular fa-square-check" style={{ color: "#23C474" }}></i>
-									{task.label}
-									<span onClick={(e) => handleDelete(index)}>
-										<i className="fa-solid fa-delete-left" style={{ color: "#FF0000"}}></i>
-									</span>
 
+								<i className="task-check fa-regular fa-square-check" style={{ color: "#23C474" }}></i>
+								<div className="task-label">
+									{task.label}
 								</div>
-							</div>
-						))}
+								<span onClick={(e) => handleDelete(index)}>
+									<i className="task-erase fa-solid fa-delete-left" style={{ color: "#FF0000" }}></i>
+								</span>
+
+							</div>))
+						}
 
 					</div>
+
+					<p className="counter"> {
+						todoList.length === 0 ? null :
+							todoList.length === 1 ? `${todoList.length} item` :
+								`${todoList.length} items`
+					}
+					</p>
+
+
 				</div>
 			) : (
-				<p > try adding some tasks you wish to do!</p>
-			)}
-		</div>
+				<p className="intro">  add some tasks you wish to do
+					<i className="fa-regular fa-pen-to-square fa-beat fa-xs ms-2"></i> </p>
+
+			)
+			}
+		</div >
 	);
 };
 export default YourTodoList;
